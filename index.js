aa613e259467261a56a71fae9cac2696c4e43ddf
Andrea29-18 <aavp0316@gmail.com>
2025-02-02 19:24:36 -0600

    Feat: Declaración

---
+++ index.js
@@ -0,0 +1,17 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function () {
+    window.location.href = 'respuesta.html'; // Redirige a la nueva página
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+    noBtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
+});
