1baa2bdc3ac8e05441f4a25fe3422c67df78eec0
Andrea29-18 <aavp0316@gmail.com>
2025-02-02 19:54:15 -0600

    fix: En movil no sirve la musica

--- respuesta.js
+++ respuesta.js
@@ -1,24 +1,3 @@
 function cerrarVentana() {
     window.location.href = 'index.html';
 }
-
-
-// Control de la música
-const music = document.getElementById('backgroundMusic');
-const musicBtn = document.getElementById('musicBtn');
-
-musicBtn.addEventListener('click', function () {
-    if (music.paused) {
-        music.play();
-        musicBtn.textContent = '🎵 Pausar música';
-    } else {
-        music.pause();
-        musicBtn.textContent = '🎵 Reproducir música';
-    }
-});
-
-// Reproducir música automáticamente al cargar la página (opcional)
-window.onload = function () {
-    music.play();
-    musicBtn.textContent = '🎵 Pausar música';
-};
